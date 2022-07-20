58be87ed8057164f4907a76ab0dab83c8c3afbbc
Kiasu <pogonyalin@mail.ru>
2022-07-20 22:00:05 +0300

    Homework 3 is done!

---
+++ Homework3.py
@@ -0,0 +1,50 @@
+# Задача 1. Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
+'''
+inpList = ["qwe", "asd", "234", "zxc", "qwe",
+             "ertqwe", "123", "ячс", "фыв", "567"]
+
+def FindNumber(inp: list):
+    outp = []
+    for item in inp:
+        if item.isdigit():
+            outp.append(item)
+    return outp
+
+
+outpList = FindNumber(inpList)
+print(f"Checklist {inpList}", end = " ")
+
+if len(outpList) > 0:
+    print(f"Have a number {' '.join(outpList)}")
+else:
+    print("Dont have a number")
+'''
+
+# 2. Напишите программу, которая определит позицию второго вхождения строки в списке либо сообщит, что её нет.
+'''
+test = [
+    (["qwe", "asd", "zxc", "qwe", "ertqwe"], "qwe"),
+    (["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"], "йцу"),
+    (["йцу", "фыв", "ячс", "цук", "йцукен"], "йцу"),
+    (["123", "234", 123, "567"], "123"),
+    ([], "123")
+]
+
+
+def FindSecPos(input: list, findValue: str) -> int:
+    count = 0
+    for i in range(len(input)):
+        if input[i] == findValue:
+            count += 1
+        if count == 2:
+            return i
+    return -1
+
+
+for _ in test:
+    secPos = FindSecPos(_[0], _[1])
+    if secPos == -1:
+        print(f"In list {_[0]} the second occurrence of the element {_[1]} dont have")
+    else:
+        print(f"In list {_[0]} the second occurrence of the element {_[1]}  - {secPos}")
+'''
